64df1cd6fe168e82543a9359c8cdcad2b65be266
wick3dr0se <wick3dr0se@protonmail.com>
2023-07-16 15:38:16 -0500

    rework output and fix pathing to math.sh

--- lib/core/math.sh
+++ lib/core/math.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-
 ## Arithmetic
+
 math() { # do advanced arithmetic in precedence from C
-  "${PWD}"/lib/core/arithmetic < <(echo "$@")
+  local output="$(${BASH_SOURCE[0]%math.sh}/arithmetic <<<"$@")"
+
+  while [[ $output =~ 0$ ]]; do
+    output="${output%0}"
+  done
+
+  printf '%s\n' "${output%.}"
 }
