1c99d9113e932e8028ede994ddbf32ee9ef70bc6
wick3dr0se <wick3dr0se@protonmail.com>
2023-08-25 03:33:47 +0000

    rewrite sgr; make vars local; refactor

--- lib/std/ansi.sh
+++ lib/std/ansi.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 ## ANSI
-declare -gA buffer cursor erase move
+declare -gA buffer cursor erase move color mode
 
-buffer=( # virtual terminal // control the terminal
+# vt100 escape sequences
+buffer=(
   ['alt']='[?1049h'
   ['main']='[?1049l'
 )
@@ -37,64 +38,69 @@ move=(
   ['down-scroll']=T
 )
 
-vt(){
-  unset seq
+# select graphic rendition color codes
+color=(
+  ['black']=0
+  ['red']=1
+  ['green']=2
+  ['yellow']=3
+  ['blue']=4
+  ['purple']=5
+  ['cyan']=6
+)
+
+mode=(
+  ['bold']=1
+  ['dim']=2
+  ['italic']=3
+  ['underlline']=4
+  ['blink']=5
+  ['rapid-blink']=6
+  ['inverse']=7
+  ['hidden']=8
+)
+
+vt(){  # virtual terminal // control the terminal
+  local vt100 x y
 
   for _; do
-    if [[ $_ == 'buffer:'*&& ${buffer[${_#buffer:}]?} ]]; then
-      seq+=("${buffer[${_#buffer:}]}")
-    elif [[ $_ == 'cursor:'*&& ${cursor[${_#cursor:}]?} ]]; then
-      seq+=("${cursor[${_#cursor:}]}")
-    elif [[ $_ == 'erase:'*&& ${erase[${_#erase:}]?} ]]; then
-      seq+=("${erase[${_#erase:}]}")
-    elif [[ $_ == 'move:'* ]]; then
-      [[ $_ =~ [0-9]$ ]]|| : "$_:1"; foo="${_#move:}"; : "${foo#*:}"
+    case $_ in
+      buffer:*) vt100+=("${buffer[${_#buffer:}]?}");;
+      cursor:*) vt100+=("${cursor[${_#cursor:}]?}");;
+      erase:*) vt100+=("${erase[${_#erase:}]?}");;
+      move:*)
+        [[ $_ =~ [0-9]$ ]]|| : "$_:1"
+        foo="${_#move:}"; : "${foo#*:}"
 
-      [[ ${move[${foo%%:*}]?} ]]&&{
-        [[ $_ =~ [0-9]+:[0-9]+ ]]&&{ x="${_%:*}" y="${_#*:}"; : "$y;${x}"; }
-        seq+=("[$_${move[${foo%%:*}]}")
-      }
-    else
-      printf 'Invalid argument!'
-      return 1
-    fi
+        [[ $_ =~ [0-9]+:[0-9]+ ]]&&{
+          x="${_%:*}" y="${_#*:}"; : "$y;${x}"
+        }
+        
+        vt100+=("[$_${move[${foo%%:*}]?}")
+      ;;
+      *)
+        printf 'Invalid argument!'
+        return 1
+      ;;
+    esac
   done
 
-  echo "${seq[@]}"
-  printf '\e%s' "${seq[@]}"
+  printf '\e%s' "${vt100[@]}"
 }
 
-sgr(){ # select graphic rendition // decorate text
+sgr(){ # select graphic rendition // style/color strings
   local sgr
-  local -A colors=(
-    # foreground or background
-    [fg]=3 [bg]=4
-    # colors
-    [black]=0 [white]=7
-    [red]=1 [green]=2 [yellow]=3
-    [blue]=4 [purple]=5 [cyan]=6
-
-  )
-
-  local -A modes=(
-    # modes
-    [bold]=1 [dim]=2 [italic]=3[underline]=4
-    [blink]=5 [rapid-blink]=6 [inverse]=7 [hidden]=8
-  )
 
   for _; do
-    if [[ ${colors[${_#*:}]-}&& $_ =~ ^[fb]g: ]]; then
-      printf '\e[%d%dm' "${colors[${_%%:*}]}" "${colors[${_#*:}]}"
-    elif [[ ${colors[$_]-} ]]; then
-      printf '\e[3%dm' "${colors[$_]}"
-    elif [[ ${modes[$_]-} ]]; then
-      printf '\e[%dm' "${modes[$_]}"
-    else
-      sgr+=("$_")
-    fi
+    case $_ in
+      bg:*) sgr+=("\e[4${color[${_#bg:}]?}m");;
+      fg:*) sgr+=("\e[3${color[${_#fg:}]?}m");;
+      mode:*) sgr+=("\e[${mode[${_#mode:}]?}m");;
+      *) sgr+=("$_\e[m");;
+    esac
   done
-  
-  printf '%s\e[m\n' "${sgr[*]}"
+
+  printf '%b' "${sgr[@]}"
 }
 
 # wipe terminal screen
