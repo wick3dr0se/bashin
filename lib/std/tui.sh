0383ec6076c577be7684dfc47a9ea6c01635af81
wick3dr0se <wick3dr0se@protonmail.com>
2023-09-08 18:25:55 +0000

    rewrite read_keys & rename term_size

--- lib/std/tui.sh
+++ lib/std/tui.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Terminal User Interface
 
-term_size(){ # outputs to $ROWS & $COLS
+get_term_size(){ # outputs to $ROWS & $COLS
   IFS='[;' read -sp $'\e7\e[999;999H\e[6n\e8' -d R -rs _ ROWS COLS
 }
 
@@ -9,12 +9,15 @@ get_cursor_pos(){ # outputs to $ROW & $COL
   IFS='[;' read -p $'\e[6n' -d R -rs _ ROW COL _
 }
 
-read_keys(){ # read keyboard/mouse input; outputs to $KEY
+read_keys(){ # read keyboard/mouse input; outputs to $KEY and $CURSOR if manipulated
   read -rsn1 KEY
-  [[ $KEY == $'\e' ]]&&{
-    read -rsn2 KEY
-    [[ $KEY == "[M" ]]&& { read -rsn3; KEY="$KEY$REPLY"; }
-  }
+  [[ $KEY == $'\e' ]]&& read -rsn2 KEY
   
-  [[ $KEY ]]&& :
+  case $KEY in
+    [kKwW]|'[A') ((ROW--));;
+    [jJsS]|'[B') ((ROW++));;
+    [lLdD]|'[C') ((COL++));;
+    [hHaA]|'[D') ((COL--));;
+    '[M') read -rsn3; KEY="$KEY$REPLY";;
+  esac
 }
