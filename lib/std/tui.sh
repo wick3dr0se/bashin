061ba1c9845461c56db4838371c853ad5183e9b8
wick3dr0se <wick3dr0se@protonmail.com>
2023-09-09 01:05:04 +0000

    add echo off/on procs and remove cursor movement

--- lib/std/tui.sh
+++ lib/std/tui.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Terminal User Interface
 
+echo_off(){
+  stty -echo
+}
+
+echo_on(){
+  stty echo
+}
+
 get_term_size(){ # outputs to $ROWS & $COLS
   IFS='[;' read -sp $'\e7\e[999;999H\e[6n\e8' -d R -rs _ ROWS COLS
 }
@@ -9,15 +17,8 @@ get_cursor_pos(){ # outputs to $ROW & $COL
   IFS='[;' read -p $'\e[6n' -d R -rs _ ROW COL _
 }
 
-read_keys(){ # read keyboard/mouse input; outputs to $KEY and $CURSOR if manipulated
+read_keys(){ # read keyboard/mouse input; modifies $ROW/$COL & outputs DIRECTION
   read -rsn1 KEY
   [[ $KEY == $'\e' ]]&& read -rsn2 KEY
-  
-  case $KEY in
-    [kKwW]|'[A') ((ROW--));;
-    [jJsS]|'[B') ((ROW++));;
-    [lLdD]|'[C') ((COL++));;
-    [hHaA]|'[D') ((COL--));;
-    '[M') read -rsn3; KEY="$KEY$REPLY";;
-  esac
+  [[ $KEY == '[M' ]]&& read -rsn3; KEY="$KEY$REPLY"
 }
