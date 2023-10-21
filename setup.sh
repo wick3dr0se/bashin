be4ab9f8c89294ed3ffc219ee70cd907b02c7040
wick3dr0se <wick3dr0se@protonmail.com>
2023-10-21 15:53:08 +0000

    source to .bash_profile instead of .bashrc

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# This is just a script to fetch bashin and source it globally
-# Check for the $BASHIN env before program execution, execute this setup script accordingly
+# This is just a script to fetch bashin and source (append) it globally from .bash_profile
+# This script can be called to make your scripts portable with bashin; Check for $BASHIN env. If doesn't exist execute this script remotely. See below
+# https://github.com/wick3dr0se/bashin/tree/main#auto-setup
 
 (( BASHIN ))&&{
   printf 'Bashin already exist on system!\n'
@@ -11,14 +12,14 @@ API='https://api.github.com/repos'
 AUTHOR='wick3dr0se'
 REPO='bashin'
 
-# create a hidden directory for bashin
-mkdir ~/.bashin
+# create a hidden directory for bashin in ~/
+mkdir "$HOME/.bashin"
 
-# download bashin and extract
-curl -L "$API/$AUTHOR/$REPO/tarball" | tar -xzC ~/.bashin --strip=1
+# download bashin and extract to ~/.bashin
+curl -L "$API/$AUTHOR/$REPO/tarball" | tar -xzC "$HOME/.bashin" --strip=1
 
-# append to .bashrc
-printf '\n. ~/.bashin/bashin\n' >>~/.bashrc
+# append sourcing ~/.bashin/bashin to ~/.bash_profile
+printf '\n. ~/.bashin/bashin\n' >>"/$HOME/.bash_profile"
 
-# source bashrc, in turn sourcing bashin
-. ~/.bashrc
+# source bash_profile, in turn sourcing bashin
+. "$HOME/.bash_profile"
