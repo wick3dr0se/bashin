e9dc95449d2ac75e8241528b5e6a65348d037077
wick3dr0se <wick3dr0se@protonmail.com>
2024-01-18 03:29:15 -0500

    switch to `shopt` to prevent issues when trapping window resize events

--- lib/std/tui.sh
+++ lib/std/tui.sh
@@ -9,15 +9,16 @@ echo_on(){
   stty echo
 }
 
-get_term_size(){ # outputs to $ROWS & $COLS
-  IFS='[;' read -sp $'\e7\e[999;999H\e[6n\e8' -d R -rs _ ROWS COLS
+get_term_size(){ # outputs to $LINES & $COLUMNS
+  shopt -s checkwinsize; (:;:)
+  ROWS="$LINES" COLS="$COLUMNS"
 }
 
-get_cursor_pos(){ # outputs to $ROW & $COL
+get_cursor_pos(){ # outputs to $LINE & $COLUMN
   IFS='[;' read -p $'\e[6n' -d R -rs _ ROW COL _
 }
 
-read_keys(){ # read keyboard/mouse input; modifies $ROW/$COL & outputs DIRECTION
+read_keys(){ # read keyboard/mouse input
   read -rsn1 KEY
   [[ $KEY == $'\e' ]]&& read -rsn2 KEY
   [[ $KEY == '[M' ]]&& read -rsn3; KEY="$KEY$REPLY"
